00b8b7faf14ded96caf5ae547ae24ad5aa84f446
papajorgioster@gmail.com <foquinha44>
2017-12-06 18:24:06 +0000

    Add UI Scaler for diferent Screen resolutions

---
+++ Assets/Scripts/UIScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour {
+
+
+    Vector3 initialScale;
+
+    private void Start()
+    {
+        initialScale = this.GetComponent<RectTransform>().localScale;
+    }
+
+    void Update () {
+        float proportionRatio = ((1080.0f * 100 / Screen.height) / 100);
+
+        this.transform.localScale = 
+            new Vector3((Screen.width / 1920.0f) * initialScale.x * proportionRatio, (Screen.height / 1080.0f) * initialScale.y * proportionRatio, 1.0f);
+    }
+}
